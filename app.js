ecbf07775d52efed25e93da76ec697d85296f044
sukhrob22 <ergashevsuxrob986@gmail.com>
2022-03-18 23:22:57 +0500

    Countdown project js

---
+++ app.js
@@ -0,0 +1,98 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate()
+
+// let futureDAte = new Date(2022, 2, 19, 10, 00, 0);
+
+const futureDAte = new Date(tempYear, tempMonth, tempDay +20, 11, 30, 0)
+
+const year = futureDAte.getFullYear();
+const hours = futureDAte.getHours();
+const minutes = futureDAte.getMinutes();
+
+let month = futureDAte.getMonth();
+month = months[month];
+const date = futureDAte.getDate();
+
+const weekday = weekdays[futureDAte.getDay()];
+
+giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year}
+   ${hours}:${minutes}am`;
+
+//futer time in ms
+const futureTime = futureDAte.getTime();
+
+function getReminingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  // 1s = 1000ms
+  //1m = 60s
+  // 1hr = 60min
+  // 1d = 24hr
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  // calcucalte all values
+  let days = t / oneDay;
+  days = Math.floor(days);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  //set values array;
+  const valuse = [days, hours, minutes, seconds];
+
+  
+  items.forEach(function (item, index) {
+      item.innerHTML = format(valuse[index]);
+    });
+
+
+    function format(item) {
+      if (item < 10) {
+        return (item = `0${item}`);
+      }
+      return item;
+    }
+    
+  if(t < 0){
+      clearInterval(countdown);
+      deadline.innerHTML = `<h4 class= "expired"> sorrym this
+       giveaway has expired</h4>`
+  }
+}
+// countdown
+let countdown = setInterval(getReminingTime, 1000)
+getReminingTime();
